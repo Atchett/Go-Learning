bb425d6f427c357feb9d37e8eb0b9a3d06be0cac
Atchett <atchett@users.noreply.github.com>
2017-08-02 19:31:25 +0100

    adding the variables lessons

---
+++ 03_variables/01_shorthand/01/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 10
+	b := "golang"
+	c := 4.17
+	d := true
+
+	// output the value of the variable (%v)
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+}
---
+++ 03_variables/02_var_zero_value/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// declare to zero value i.e. not initialized
+	var a int
+	var b string
+	var c float64
+	var d bool
+
+	// output the type of the value (%T)
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+}
