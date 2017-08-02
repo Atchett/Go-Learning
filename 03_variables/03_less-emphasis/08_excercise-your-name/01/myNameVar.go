86566771cd4934821ccdbef6c0308678879ceac7
Atchett <atchett@users.noreply.github.com>
2017-08-02 21:17:05 +0100

    adding more variables lessons

---
+++ 03_variables/03_less-emphasis/08_excercise-your-name/01/myNameVar.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+var name = "John"
+
+func main() {
+	fmt.Println("Hello ", name)
+}
---
+++ 03_variables/03_less-emphasis/08_excercise-your-name/04/myNameVar.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var name = `John` // back-ticks work like double-quotes
+	fmt.Println("Hello ", name)
+}
