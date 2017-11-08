556a32f7cb1ee46a6d2abc390fc2d2f0697c1e92
John Spurgin <Atchett@users.noreply.github.com>
2017-11-08 20:11:25 +0000

    Line endings

--- 21_interfaces/js-solutions/02/main.go
+++ 21_interfaces/js-solutions/02/main.go
@@ -10,6 +10,14 @@ func main() {
 	// sort forward and output
 	sort.Strings(s)
 	fmt.Println(s)
+	//alternate method
+	// sort.StringSlice implements the Interface interface on the s string slice so that we can now use .Sort
+	sort.StringSlice(s).Sort()
+	fmt.Println(s)
+	// another method
+	// pass in the Interface interface which sort.StringSlice(s) implements
+	sort.Sort(sort.StringSlice(s))
+	fmt.Println(s)
 	// sort reverse and output
 	// note: use of stringSlice as this implements the sort.Interface interface
 	sort.Sort(sort.Reverse(sort.StringSlice(s)))
